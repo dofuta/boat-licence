67c64ee5258441b164598ca01e6d3daf23eaa744
dofuta <>
2018-10-18 12:35:27 +0900

    Add alert for update action in day_details_controller

--- app/controllers/day_details_controller.rb
+++ app/controllers/day_details_controller.rb
@@ -2,11 +2,13 @@ class DayDetailsController < ApplicationController
   def create
     daydetail = DayDetail.new(day_detail_params)
     unless daydetail.save(day_detail_params)
-      flash[:alert] = "エラー！備考を保存できませんでした"
+      flash[:alert] = "エラー！備考を保存できませんでした。"
     end
   end
   def update
-    DayDetail.find(params[:id]).update(day_detail_params)
+    unless DayDetail.find(params[:id]).update(day_detail_params)
+      flash[:alert] = "エラー！変更を保存できませんでした。"
+    end
   end
 
 private
