73710baf1c1d1b8bf248447c8ab0404d365d5e3e
dofuta <>
2018-10-22 15:28:11 +0900

    Modify validation

--- app/models/gakka_passed_number.rb
+++ app/models/gakka_passed_number.rb
@@ -1,4 +1,4 @@
 class GakkaPassedNumber < ApplicationRecord
-  validates :siken_id, uniqueness: true
+  validates :org_siken_id, uniqueness: true
   belongs_to :exam, primary_key: "org_siken_id", foreign_key: "org_siken_id"
 end
